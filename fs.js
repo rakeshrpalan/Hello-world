6295c8bd9eca0e7995d744fbc450bf01ab982ce8
Rakesh <rakesh.r@niveussolutions.com>
2022-11-22 15:04:53 +0530

    second commit

--- fs.js
+++ fs.js
@@ -10,14 +10,5 @@ fs.mkdirSync(folderName);
 console.log(fs.existsSync(folderName));
 
 
-// const folderpath = `./${folderName}`;
-
-// fs.writeFile('index.html','<h1>hello</h1>',(err) =>{
-//     if(err) throw err;
-//     console.log('file created');
-// });
-
-// let val1 = fs.readdirSync('folder exists');
-// console.log(val1);
 
 
